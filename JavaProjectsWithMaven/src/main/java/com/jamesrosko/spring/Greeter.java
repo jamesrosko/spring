61a7e70f75aa2b55bbb6250eef3cd79f0758a5fe
James <jamesrosko@gmail.com>
2015-07-30 07:51:31 -0700

    use the maven dependency

--- JavaProjectsWithMaven/src/main/java/com/jamesrosko/spring/Greeter.java
+++ JavaProjectsWithMaven/src/main/java/com/jamesrosko/spring/Greeter.java
@@ -1,9 +1,14 @@
 package com.jamesrosko.spring;
 
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeConstants;
+
 public class Greeter {
 
-	public String sayHello() {
-		return "Hello World";
-	}
+  public String sayHello() {
+    DateTime.Property pDoW = new DateTime().dayOfWeek();
+    String dayOfWeekAsText = pDoW.getAsText();
+    return "Hello World - " + dayOfWeekAsText;
+  }
 
 }
