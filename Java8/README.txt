b00cedbe1c5f5bd221dee6a2d5bb738936f3b1a6
James <jamesrosko@gmail.com>
2015-11-13 10:36:39 -0800

    inital commit of Java8, includes default method in interface

---
+++ Java8/README.txt
@@ -0,0 +1,3 @@
+
+Following the guide from:
+https://github.com/winterbe/java8-tutorial#default-methods-for-interfaces
---
+++ Java8/src/test/java/com/jamesrosko/java/defaultmethod/FormulaTest.java
@@ -0,0 +1,23 @@
+package com.jamesrosko.java.defaultmethod;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+public class FormulaTest {
+
+  private Formula formula = new Formula() {
+    @Override
+    public double calculate(int a) {
+      return sqrt(a * 100);
+    }
+  };
+
+  @Test
+  public void test() {
+    assertThat(formula.calculate(100), equalTo(100.0));
+    assertThat(formula.sqrt(16), equalTo(4.0));
+  }
+
+}
