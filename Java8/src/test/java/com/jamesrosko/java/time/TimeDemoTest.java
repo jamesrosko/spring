18dc0c45f40be5944dc2104c893466b151e1bb48
James <jamesrosko@gmail.com>
2015-11-17 13:37:21 -0800

    fix TimeDemoTest with formatter

--- Java8/src/test/java/com/jamesrosko/java/time/TimeDemoTest.java
+++ Java8/src/test/java/com/jamesrosko/java/time/TimeDemoTest.java
@@ -2,6 +2,7 @@ package com.jamesrosko.java.time;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
@@ -12,22 +13,30 @@ public class TimeDemoTest {
 
   @Test
   public void test() {
-    
+
     System.out.println(Instant.EPOCH);
-    
+
     System.out.println(Instant.MAX);
-    
+
     Duration thenToNow = Duration.between(Instant.EPOCH, Instant.now());
     System.out.println(thenToNow);
-   
+
     System.out.println(LocalTime.now());
     Instant now = Instant.now();
     Instant tomorrow = Instant.now().plus(1, ChronoUnit.DAYS);
     System.out.println(now);
     System.out.println(tomorrow);
-    
-    String formattedNow = DateTimeFormatter.BASIC_ISO_DATE.format();
-    System.out.println(formattedNow);
+
+  }
+
+  @Test
+  public void testFormatter() {
+    LocalDate now = LocalDate.now();
+    String text = now.format(DateTimeFormatter.BASIC_ISO_DATE);
+    LocalDate parsedDate = LocalDate.parse(text, DateTimeFormatter.BASIC_ISO_DATE);
+    System.out.println(text);
+    System.out.println(parsedDate);
+
   }
 
 }
